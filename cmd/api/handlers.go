4eab3bb46e431a12d025cc9218f1cf7d147793a5
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2026-01-24 00:20:25 -0500

    Implement logging functionality with HTTP handlers and MongoDB integration

---
+++ cmd/api/handlers.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/JuD4Mo/logger-service/data"
+)
+
+type JSONPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
+	//read json into var
+
+	var requestPayload JSONPayload
+
+	_ = app.readJson(w, r, &requestPayload)
+
+	//insert data
+	event := data.LogEntry{
+		Name: requestPayload.Name,
+		Data: requestPayload.Data,
+	}
+
+	err := app.Models.LogEntry.Insert(event)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	resp := jsonResponse{
+		Error:   false,
+		Message: "logged",
+	}
+
+	app.writeJSON(w, http.StatusAccepted, resp)
+}
